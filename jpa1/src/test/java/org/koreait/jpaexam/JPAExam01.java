95d35982daf67983285da7d3a733d9468293e796
hyuk0u0 <hyuk0u0@naver.com>
2023-05-30 15:40:10 +0900

    EntityManager 기본 메서드

--- jpa1/src/test/java/org/koreait/jpaexam/JPAExam01.java
+++ jpa1/src/test/java/org/koreait/jpaexam/JPAExam01.java
@@ -1,6 +1,7 @@
 package org.koreait.jpaexam;
 
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.koreait.entities.Member;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +12,24 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @TestPropertySource(locations="classpath:application-test.properties")
 @Transactional
-public class JAPExam01 {
+public class JPAExam01 {
 
     @Autowired
     private EntityManager em;
+
+    @BeforeEach
+    void init() {
+        Member member = new Member();
+        member.setUserNo(1L);
+        member.setUserId("user01");
+        member.setUserNm("사용자01");
+        member.setUserPw("123456");
+
+        em.persist(member); // 영속성 상태 변화 감지
+        em.flush(); // 변화 상태 -> DB에 반영
+
+        em.detach(member);
+    }
 
     @Test
     void ex01() {
@@ -27,8 +42,22 @@ public class JAPExam01 {
         em.persist(member); // 영속성 상태 변화 감지
         em.flush(); // 변화 상태 -> DB에 반영
 
-        member.setUserNm("(수정)사용자01"); // 수정
+        em.detach(member);  // 영속성 분리 : 변화 감지 X
 
+        member.setUserNm("(수정)사용자01"); // 수정
         em.flush(); // 변화 상태 -> DB에 반영
+
+        em.merge(member);  // 분리된 영속성 -> 영속 상태 : 변화 감지
+        em.flush();
+
+
+//        em.remove(member); // 영속성 상태 -> 제거 상태
+//        em.flush(); // 변화 상태 -> DB 반영(DELETE)
+    }
+
+    @Test
+    void ex02() {
+        Member member = em.find(Member.class, 1L);
+        System.out.println(member);
     }
 }
