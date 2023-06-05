6e6ca3cce8b85aef6b26b2fe440f7f5fbb995948
hyuk0u0 <hyuk0u0@naver.com>
2023-06-05 17:52:31 +0900

    시큐리티

--- board/src/main/java/org/koreait/configs/SecurityConfig.java
+++ board/src/main/java/org/koreait/configs/SecurityConfig.java
@@ -1,23 +1,46 @@
 package org.koreait.configs;
 
+import org.koreait.controllers.member.LoginFailureHandler;
+import org.koreait.models.member.LoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin(f -> f
+                        .loginPage("/member/login")
+                        .usernameParameter("userId")
+                        .passwordParameter("userPw")
+                        //.defaultSuccessUrl("/")  // 로그인 성공시 유입될 URL
+                        //.failureUrl("/member/login") // 로그인 실패시 유입될 URL
+                        .failureHandler(new LoginFailureHandler())
+                        .successHandler(new LoginSuccessHandler())
+                )
+                .logout(f -> f
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                        .logoutSuccessUrl("/member/login") // 로그아웃 성공시 URL
+                );
+
+        http.authorizeHttpRequests(f -> f
+                .requestMatchers("/mypage/**").authenticated() // 로그인한 회원만 접근 가능한 URL
+                .requestMatchers("/admin/**").hasAuthority("ADMIN") // 관리자만 접근 가능한 페이지
+                .anyRequest().permitAll()
+        );
 
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
+
         return new BCryptPasswordEncoder();
     }
 }
--- board/src/main/java/org/koreait/repositories/MemberRepository.java
+++ board/src/main/java/org/koreait/repositories/MemberRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslPredicateExecutor<Member> {
 
+    Member findByUserId(String userId);
+
     default boolean exists(String userId) {
         QMember member = QMember.member;
 
